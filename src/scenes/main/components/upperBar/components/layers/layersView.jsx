9b05c61a68a804954a3942572c300cb0c8d8bc11
Yossi Yadgar <yossi.yadgar@devalore.com>
2017-01-01 10:48:05 +0200

    Now selected Layers is save in the Layers component state

--- src/scenes/main/components/upperBar/components/layers/layersView.jsx
+++ src/scenes/main/components/upperBar/components/layers/layersView.jsx
@@ -21,6 +21,9 @@ const componentStyle = {
         listStyle: 'none',
         marginRight: '15px',
     },
+    liSelected : {
+        border: '1px solid white'
+    },
     ul : {
         marginTop: '16px',
         display: 'inline-flex',
@@ -29,18 +32,47 @@ const componentStyle = {
 
 export default class Layers extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLayerIndexes: []
+        };
+
+        //bindings
+        this.onLayerClicked = this.onLayerClicked.bind(this);
+    }
+
+    onLayerClicked(layerIndex) {
+        const isNotExist = -1;
+        const layerIndexInSelectedArray = this.state.selectedLayerIndexes.indexOf(layerIndex);
+        const isSelectedLayerWasSelectedBefore = layerIndexInSelectedArray != isNotExist;
+        const updatedSelected = this.state.selectedLayerIndexes;
+        
+        this.props.actions.setActiveLayer(layerIndex);
+        if(isSelectedLayerWasSelectedBefore) {
+            updatedSelected.splice(layerIndexInSelectedArray, 1); // if selected before so remove selection   
+        }
+        else {
+            updatedSelected.push(layerIndex);
+        }
+        
+        this.setState({selectedLayerIndexes: updatedSelected});
+    }
+
     render() {
         return (
             <div style = {componentStyle.containerDiv}>
                 <ul style = {componentStyle.ul}>
-                    {this.props.layers.map((layer, idx) => 
-                    <li key={idx} style = {componentStyle.li}>
-                        <img style={imageStyle} 
-                            id = {Guid.create()}
-                            src={layer.imgUrl}
-                            onClick = {() =>  this.props.actions.setActiveLayer(idx)}
-                        />
-                    </li>)}
+                    {
+                        this.props.layers.map((layer, idx) => 
+                            <li key={idx} style = {componentStyle.li}>
+                                <img style={imageStyle} 
+                                    id = {Guid.create()}
+                                    src={layer.imgUrl}
+                                    onClick = {() =>  this.onLayerClicked(idx)}
+                                />
+                            </li>)
+                    }
                 </ul>
             </div>
         );
