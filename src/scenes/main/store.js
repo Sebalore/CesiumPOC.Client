ce0c12b021b190173adb3e6293ec6256111643b5
LAPTOP-ITAG1OLF\Sebastian Katz <sebastian.katz@devalore.com>
2017-01-04 16:44:25 +0200

    Added UAV properties

--- src/scenes/main/store.js
+++ src/scenes/main/store.js
@@ -8,13 +8,11 @@ import { createLinearCoordinatesGenerator } from './services';
 import {resources} from '../../shared/data/resources';
 
 const initialViewState = {
-  entityTypes: [
-        {
+  entityTypes: [{
       name: resources.ENTITY_TYPE_NAMES.FLIGHT_CIRCLE_IN,
       imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.FLIGHT_CIRCLE_IN].IMG}`,
       active: true,
-      actions: [
-        {
+      actions: [{
           id: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].ACTIONS.ADD.ID,
           description: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].ACTIONS.ADD.DESC,
           imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.FLIGHT_CIRCLE_IN].ACTIONS.ADD.IMG}`
@@ -36,36 +34,32 @@ const initialViewState = {
         //   }
         // }
       ]
-    },
-    {
+    }, {
       name: resources.ENTITY_TYPE_NAMES.DMA,
       imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].IMG}`,
       active: true,
-      actions: [
-        {
+      actions: [{
           id: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].ACTIONS.ADD.ID,
           description: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].ACTIONS.ADD.DESC,
           imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.DMA].ACTIONS.ADD.IMG}`
         }
       ],
-      entities: [
-      ]
+      entities: []
     }, {
       name: resources.ENTITY_TYPE_NAMES.AIRPLANE,
       imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.AIRPLANE].IMG}`,
       active: true,
-      actions: [
-        {
+      actions: [{
           id: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.AIRPLANE].ACTIONS.ADD.ID,
           description: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.AIRPLANE].ACTIONS.ADD.DESC,
           imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.AIRPLANE].ACTIONS.ADD.IMG}`
-        }
+        },
       ],
-      entities: [
-        {
-          id: Guid.create(),
+      entities: [{
+          id: Guid.create(), //serial number on planes tail;
           cesiumId: null, //guid to be provided by cesium
           label: `${resources.ENTITY_TYPE_NAMES.AIRPLANE}: 001`,
+          missionId : null,
           position: {
             longitude: resources.MAP_CENTER.longitude - 0.075,
             latitude: resources.MAP_CENTER.latitude - 0.04,
@@ -75,6 +69,22 @@ const initialViewState = {
             image: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.AIRPLANE].ACTIONS.ADD.IMG}`,
             scale: 0.95
           },
+          uavData: {
+            orientation : {
+              yaw : null,
+              pitch: null,
+              roll : null
+            },
+            windData: {
+              direction: null,
+              speed : null
+            },
+            velocity:{
+              north : null,
+              east: null,
+              down: null
+            }
+          }, 
           gen:  createLinearCoordinatesGenerator({
             longitude: 0.002,
             latitude: 0.001,
@@ -124,15 +134,13 @@ const initialViewState = {
       name: resources.ENTITY_TYPE_NAMES.HELICOPTER,
       imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.HELICOPTER].IMG}`,
       active: true,
-      actions: [
-        {
+      actions: [{
           id: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.HELICOPTER].ACTIONS.ADD.ID,
           description: resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.HELICOPTER].ACTIONS.ADD.DESC,
           imgUrl: `${resources.IMG.BASE_URL}${resources.ENTITY_TYPES[resources.ENTITY_TYPE_NAMES.HELICOPTER].ACTIONS.ADD.IMG}`
         }
       ],
-      entities: [
-        {
+      entities: [{
           id: Guid.create(),
           cesiumId: null, //guid to be provided by cesium
           label: `${resources.ENTITY_TYPE_NAMES.HELICOPTER}: 001`,
