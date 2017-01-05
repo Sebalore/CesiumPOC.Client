257e1df20d57ffdb5c34a2f0a1e98c427d9d8b8c
Yossi Yadgar <yossi.yadgar@devalore.com>
2017-01-05 14:49:49 +0200

    Now we can import inline svg code

---
+++ src/shared/images/AllImages.js
@@ -0,0 +1,16 @@
+export const Images = {
+    arrow_navigation: 
+        `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="-133 110 32 32" style="enable-background:new -133 110 32 32;" xml:space="preserve">
+        <polygon points="-130.4,131.3 -117,117.9 -103.6,131.3 -106.4,134.1 -117,123.5 -127.6,134.1 "/>
+        </svg>`,
+    icon_11: 
+        `<svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+            y="0px" viewBox="-455 256.5 73.5 88.5" style="enable-background:new -455 256.5 73.5 88.5;" xml:space="preserve">
+            <path d="M-441.7,321.5c-8.1-6.7-13.3-16.9-13.3-28.3l0,0c0-20.3,16.5-36.7,36.8-36.8l0,0c20.3,0,36.7,16.5,36.8,36.8l0,0
+            c0,11.3-5.1,21.4-13.1,28.1l-23.6,23.6L-441.7,321.5z M-418.2,324.1c17,0,30.8-13.8,30.9-30.9l0,0c0-17.1-13.8-30.8-30.9-30.9l0,0
+            c-17.1,0-30.8,13.8-30.9,30.9l0,0C-449.1,310.3-435.3,324.1-418.2,324.1L-418.2,324.1z"/>
+        </svg>`,
+};
+// use example :
+// <div dangerouslySetInnerHTML={{__html: Images.icon_11}} />
