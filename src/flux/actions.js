c975e8f972478cda69ab5e4bb5523e7f1468b697
LAPTOP-ITAG1OLF\Sebastian Katz <sebastian.katz@devalore.com>
2017-01-11 16:27:18 +0200

    Added sockets

--- src/flux/actions.js
+++ src/flux/actions.js
@@ -1,12 +1,18 @@
 import dispatcher from './dispatcher';
-import { resources } from '../shared/data/resources.js'; 
+import { resources } from '../shared/data/resources'; 
+import socketIO from 'socket.io';
+
+const socket = socketIO('http://localhost:8080');
 
 export default {
     //--------- general actions ----------------------------------
     update: (data) => dispatcher.dispatch({type: 'UPDATE', data}),
     
     // entities actions
-    [resources.ACTIONS.ADD.TYPE]: (agent, data) => dispatcher.dispatch({type: resources.ACTIONS.ADD.TYPE, agent, data}),
+    [resources.ACTIONS.ADD.TYPE]: (agent, data) => {
+        dispatcher.dispatch({type: resources.ACTIONS.ADD.TYPE, agent, data});
+        socket.emit('ADD', { data });
+    },
     [resources.ACTIONS.DELETE.TYPE]: (agent, data) => dispatcher.dispatch({type: resources.ACTIONS.DELETE.TYPE, agent, data}),
     [resources.ACTIONS.UPDATE_POSITION.TYPE]: (agent, data) => dispatcher.dispatch({type: resources.ACTIONS.UPDATE_POSITION.TYPE,  agent, data}),
     [resources.ACTIONS.SET_ENTITY_CESIUM_ID.TYPE]: (agent, data) => dispatcher.dispatch({type: resources.ACTIONS.SET_ENTITY_CESIUM_ID.TYPE,  agent, data}),
